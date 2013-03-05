614280bcdc284e4f87e6e335ab90295200508ab8
SOPORTE 2 <SOPORTE 2@SOPORTE2>
2013-03-05 15:39:42 -0600

    clase vendedores telVendedores creada

--- smartsms/grails-app/domain/smartsms/TelVendedores.groovy
+++ smartsms/grails-app/domain/smartsms/TelVendedores.groovy
@@ -4,6 +4,8 @@ class TelVendedores {
     int id
     int idVendedor
     String telefono
+    static belongsTo = [idVendedor: Vendedores]
+    
     static constraints = {
         id(blank:false, unique:true)
         idVendedor(blank:false)
