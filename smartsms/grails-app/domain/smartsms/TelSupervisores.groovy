0ea6eb5ddde37e22debf355539e5e86b28be5fd0
soporte_3 <soporte_3@soporte_3_PC>
2013-03-06 08:23:26 -0600

    modificacion

---
+++ smartsms/grails-app/domain/smartsms/TelSupervisores.groovy
@@ -0,0 +1,13 @@
+package smartsms
+
+class TelSupervisores {
+    
+    int id
+    static belongsTo=[idSupervisor:Supervisores]
+    String telefono
+    static constraints = {
+        id(blank:false, unique:true)
+        idSupervisor(blank:false)
+        telefono(blank:false)
+    }
+}
